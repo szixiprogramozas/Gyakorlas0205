c9b3a0a872a3b5a0bbba9ea96a1dab2a4b7c4357
Balogh Imre <b.imre.edu@szixi.hu>
2021-02-04 21:32:20 +0100

    Szamok lista hozzáadása

--- Gyakorlas/Program.cs
+++ Gyakorlas/Program.cs
@@ -11,7 +11,15 @@ namespace Gyakorlas
         static void Main(string[] args)
         {
 
-            Console.WriteLine("SZEVASZTOK!");
+            List<int> Szamok = new List<int> {3,5,2,63,2,1,5,7,5,3,1,51,0,94,3};
+
+            foreach (int Szam in Szamok)
+	{
+                Console.Write(Szam + " ");
+	}
+
+
+
             Console.ReadKey();
 
         }
