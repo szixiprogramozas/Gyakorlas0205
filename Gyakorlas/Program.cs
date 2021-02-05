97a65a79f7fe020198988050837483e213b30d91
Roli <keszrol@gmail.com>
2021-02-05 08:44:40 +0100

    meghívja az egyes feladatokat a program, (a 'voidot' lesz ahol át kell írni a metódusba a feladatoktól függően)

--- Gyakorlas/Program.cs
+++ Gyakorlas/Program.cs
@@ -18,9 +18,117 @@ namespace Gyakorlas
                 Console.Write(Szam + " ");
 	}
 
-
+            Console.WriteLine("\n");
+            Console.WriteLine("2. Feladat: ");
+            masodikfeladat();
+            Console.WriteLine();
+            Console.WriteLine("3. Feladat: ");
+            harmadikfeladat();
+            Console.WriteLine();
+            Console.WriteLine("4. Feladat: ");
+            negyedikfeladat();
+            Console.WriteLine();
+            Console.WriteLine("5. Feladat: ");
+            otodikfeladat();
+            Console.WriteLine();
+            Console.WriteLine("6. Feladat: ");
+            hatodikfeladat();
+            Console.WriteLine();
+            Console.WriteLine("7. Feladat: ");
+            hetedikfeladat();
+            Console.WriteLine();
+            Console.WriteLine("8. Feladat: ");
+            nyolcasfeladat();
+            Console.WriteLine();
+            Console.WriteLine("9. Feladat: ");
+            kilencesfeladat();
+            Console.WriteLine();
+            Console.WriteLine("10. Feladat: ");
+            tizesfeladat();
+            Console.WriteLine();
+            Console.WriteLine("11. Feladat: ");
+            tizenegyesfeladat();
+            Console.WriteLine();
+            Console.WriteLine("12. Feladat: ");
+            tizenkettesfeladat();
+            Console.WriteLine();
+            Console.WriteLine("13. Feladat: ");
+            tizenharmasfeladat();
+            Console.WriteLine();
+            Console.WriteLine("14. Feladat: ");
+            tizennegyesfeladat();
+            Console.WriteLine();
+            Console.WriteLine("15. Feladat: ");
+            tizenotosfeladat();
+            Console.WriteLine();
+            Console.WriteLine("16. Feladat: ");
+            tizenhatosfeladat();
 
             Console.ReadKey();
+        }
+
+        static void masodikfeladat()
+        {
+
+        }
+
+        static void harmadikfeladat()
+        {
+
+        }
+
+        static void negyedikfeladat()
+        {
+
+        }
+
+        static void otodikfeladat()
+        {
+
+        }
+        static void hatodikfeladat()
+        {
+
+        }
+        static void hetedikfeladat()
+        {
+
+        }
+        static void nyolcasfeladat()
+        {
+
+        }
+        static void kilencesfeladat()
+        {
+
+        }
+        static void tizesfeladat()
+        {
+
+        }
+        static void tizenegyesfeladat()
+        {
+
+
+        }
+        static void tizenkettesfeladat()
+        {
+
+        }
+        static void tizenharmasfeladat()
+        {
+
+        }
+        static void tizennegyesfeladat()
+        {
+
+        }
+        static void tizenotosfeladat()
+        {
+
+        }
+        static void tizenhatosfeladat()
+        {
 
         }
     }
